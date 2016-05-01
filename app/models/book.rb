c53d08c7dd7665a0c966ef1e08154e6dd6e173d8
cerevisiae <cerevisiae@kinkood.com>
2016-05-01 15:35:59 -0500

    Add genres through BookGenres and filter by genre.

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ActiveRecord::Base
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
   scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+  scope :filter, ->(genre_name){ joins(:genres).where( genres: { name: genre_name } ) if genre_name.present? }
 
   before_save :set_keywords
 
