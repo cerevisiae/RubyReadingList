f95b9e544a6d960a69d83631ca9b3444f4515b2f
cerevisiae <cerevisiae@kinkood.com>
2016-05-01 13:21:09 -0500

    Genres added to books and displayed on index.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Book.destroy_all
+Genre.destroy_all
+
+fiction = Genre.create!(name: 'Fiction')
+non_fiction = Genre.create!(name: 'Non-Fiction')
+computers = Genre.create!(name: 'Computers')
 
 Book.create!([{
                   title: "Neuromancer",
@@ -14,7 +19,8 @@ Book.create!([{
                   description: "I read this science fiction book in high school and it was one of my first major experiences with cyberpunk. It was amazing and I immediately fell in love with the genre.",
                   amazon_id: "0441569595",
                   rating: 4,
-                  finished_on: 11.years.ago
+                  finished_on: 11.years.ago,
+                  genres: [fiction, computers]
               },
               {
                   title: "Lord of Light",
@@ -22,7 +28,8 @@ Book.create!([{
                   description: "Another science fiction book I read in high school and I was blown away by the unique setting of religion being re-enacted through technology.",
                   amazon_id: "0060567236",
                   rating: 4,
-                  finished_on: 10.years.ago
+                  finished_on: 10.years.ago,
+                  genres: [fiction]
               },
               {
                   title: "Design Patterns: Elements of Reusable Object-Oriented Software",
@@ -30,7 +37,8 @@ Book.create!([{
                   description: "I'm in the middle of reading this and am enjoying learning about design patterns.",
                   amazon_id: "0201633612",
                   rating: 5,
-                  finished_on: nil
+                  finished_on: nil,
+                  genres: [non_fiction, computers]
               }])
 
 p "Created #{Book.count} books"
